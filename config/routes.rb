28a696129738755d1681c4b6d739cf2ce3a1b345
Deepak <dpoo.bang@gmail.com>
2016-05-16 18:08:21 +0530

    Add user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
   post 'login'  => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
   resources :users
+  resources :account_activation, only: [:edit]
+  resources :microposts, only: [:create, :destroy]
 end
